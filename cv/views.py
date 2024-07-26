afbf4788248962c241732d5a8f922d0e5304b8db
ant4p <enter4p@gmail.com>
2024-07-26 18:18:23 +0300

    add structure

--- cv/views.py
+++ cv/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class ShowCV(TemplateView):
+    template_name = 'cv/index.html'
--- fox4/urls.py
+++ fox4/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from cv.views import ShowCV
+from fox4 import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ShowCV.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
